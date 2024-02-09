cd87d77293e5e8617a654012351d809af0c5bdd8
anderoidy <you@example.com>
2024-02-09 17:24:37 -0300

    criado o http

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -12,13 +12,13 @@ namespace ProEventos.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class EventoController : ControllerBase
+    public class EventosController : ControllerBase
     {   
     public IEnumerable<Evento> _evento =  new Evento[] {
 
     };
         private readonly DataContext _context;
-        public EventoController(DataContext context) {
+        public EventosController(DataContext context) {
             _context = context;
         }   
 
