75e21b8113f768075c35bb1687a3055b227d9345
anderoidy <you@example.com>
2024-04-04 20:25:48 -0300

    reoganizado a estrutura de pastas

---
+++ Back/src/ProEventos.Domain/Palestrante.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.Domain
+{
+    public class Palestrante
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string MiniCurso { get; set; }
+        public string ImagemURL { get; set; }
+        public int Telefone { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<RedeSocial> RedeSociais { get; set; }
+        public IEnumerable<PalestranteEvento> PalestrantesEventos { get; set; }
+
+
+    }
+}
