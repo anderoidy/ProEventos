65dd39fff751d4798d9c76d6f27c47c7e80b459e
Ander Sistemas Web <andersistemasweb@MacBook-Air-de-Ander.local>
2024-09-27 23:48:40 -0300

    criado componente eventoDetalhe e EventoList

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -25,6 +25,8 @@ import { TituloComponent } from './shared/titulo/titulo.component';
 import { ContatosComponent } from './components/contatos/contatos.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
+import { EventoDetalheComponent } from './components/eventos/evento-detalhe/evento-detalhe.component';
+import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
 
 @NgModule({
   declarations: [
@@ -36,7 +38,9 @@ import { PerfilComponent } from './components/perfil/perfil.component';
     TituloComponent,
     ContatosComponent,
     DashboardComponent,
-    PerfilComponent
+    PerfilComponent,
+    EventoDetalheComponent,
+    EventoListaComponent
    ],
   imports: [
     BrowserModule,
--- Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -1,11 +1,4 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { ToastrService } from 'ngx-toastr';
-import { EventoService } from '../../services/evento.service';
-import { Evento } from '../../models/Evento';
-import { NgxSpinnerService } from 'ngx-spinner';
-
-
 
 @Component({
   selector: 'app-eventos',
@@ -14,74 +7,10 @@ import { NgxSpinnerService } from 'ngx-spinner';
   //providers: [EventoService]
 })
 export class EventosComponent implements OnInit {
-  modalRef?: BsModalRef;
-  public eventos: Evento[] = []
-  public eventosFiltrados: Evento[] = []
+  
+  ngOnInit(): void {
 
-  public larguraImagem = 150
-  public margemImagem = 2
-  public exibirImagem = true
-  private filtroListado = ""
-
-  public get filtroLista(): string {
-    return this.filtroListado
   }
-
-  public set filtroLista( value: string) {
-    this.filtroListado = value
-    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos
-  }
-
-  public filtrarEventos(filtrarPor: string): Evento[] {
-      filtrarPor = filtrarPor.toLocaleLowerCase();
-      return this.eventos.filter(
-        evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
-        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
-      );
-  }
-
-  constructor(
-    private eventoService: EventoService,
-    private modalService: BsModalService,
-    private toastr: ToastrService,
-    private spinner: NgxSpinnerService
-  ) { }
-
-  ngOnInit() : void {
-    this.spinner.show();
-    this.getEventos();
-  }
-
-  public alterarImagem(): void {
-    this.exibirImagem = !this.exibirImagem
-  }
-
-  public getEventos(): void {
-    this.eventoService.getEventos().subscribe({
-        next: (eventos: Evento[]) => {
-          this.eventos = eventos;
-          this.eventosFiltrados = this.eventos
-        },
-        error: (error: any) => {
-          this.spinner.hide();
-          this.toastr.error('Erro ao Carregar os Eventos', 'Erro');
-        },
-        complete: () => this.spinner.hide()
-      });
-  }
-
-    public openModal(template: TemplateRef<void>): void {
-      this.modalRef = this.modalService.show(template, { class: 'modal-sm' });
-    }
-
-    confirm(): void {
-      this.modalRef?.hide();
-      this.toastr.success('Deletado', 'O Evento foi Deletado com Sucesso!');
-    }
-
-    decline(): void {
-      this.modalRef?.hide();
-    }
 }
 
 
